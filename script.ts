dc29c90818a63f20ea957e43f1ca4650ef285a1b
dua131 <duaeseharduaesehar@gmail.com>
2024-10-02 15:49:25 +0500

    Add files via upload

--- script.ts
+++ script.ts
@@ -1,10 +1,41 @@
-const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement
-const skills = document.getElementById('skills') as HTMLElement
+//Get references to the form and display area
+const form =document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
 
-toggleButton.addEventListener('click',()=>{
-    if (skills.style.display === 'none'){
-        skills.style.display ='block'
-    } else{
-        skills.style.display = 'none'
+//Handle form submission
+form.addEventListener('submit',(event:Event) =>{
+    event.preventDefault(); // prevent page reload
+
+    //Collect input value
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone =(document.getElementById('phone') as HTMLInputElement).value
+    const education =(document.getElementById('education') as HTMLInputElement).value
+    const  experience =(document.getElementById('experience') as HTMLInputElement).value
+    const skills =(document.getElementById('skills') as HTMLInputElement).value
+
+    //Generte the resume content dynamiclly
+    const resumeHTML = `
+    <h2><b<Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+    <p><b>Email:</b>${email}</P>
+    <p><b>Phone:</b>${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</p>
+    `;
+
+    //Display the generated resume
+    if(resumeDisplayElement){
+      resumeDisplayElement.innerHTML = resumeHTML;
+    }else{
+        console.error('The resume display element is missing.');
     }
 });
